841d3e0d92832164a8347568b09b95c9ce1c9d63
rish6ix <rish6ix@rish6ixs-MacBook-Pro.local>
2022-10-04 18:24:57 +0530

    fix

--- src/pages/NewPages/DAONoAccess.tsx
+++ src/pages/NewPages/DAONoAccess.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import "../../styles/pages/DAOSuccess.css";
 import "../../styles/Global.css";
-import frameicon from "../../assets/svg/frame.svg";
+import frameicon from "../../assets/svg/Frame.svg";
 import GroupEnjoy from "../../assets/svg/GroupEnjoy.svg";
 import { colors } from "assets/colors";
 import { Colorstype } from "types/UItype";
