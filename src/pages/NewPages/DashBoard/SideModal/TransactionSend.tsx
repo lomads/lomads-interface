8fa11c92db176453614cf02773df2a0520a58ed3
rish6ix <rish6ix@rish6ixs-MacBook-Pro.local>
2022-12-02 14:08:15 +0530

    bugfix

--- src/pages/NewPages/DashBoard/SideModal/TransactionSend.tsx
+++ src/pages/NewPages/DashBoard/SideModal/TransactionSend.tsx
@@ -122,8 +122,8 @@ const TransactionSend = (props: IselectTransactionSend) => {
 											/> */}
 										<NumberInput onChange={(e) => {
 													props.setRecipient.current[index].amount = `${+e}`;
-												}}  style={{ width: (64 + 50), height: 50, borderRadius: '10px 10px 10px 10px', boxShadow: 'inset -1px 0px 4px rgba(27, 43, 65, 0.1)' }} step={1} min={0}>
-											<NumberInputField className='input' style={{ padding: 0, textAlign: "center", height: 50, width: 64, backgroundColor: '#F5F5F5', borderRadius: '10px 0px 0px 10px', borderWidth: 0 }} />
+												}}  style={{ width: (54 + 50), height: 50, borderRadius: '10px 10px 10px 10px', boxShadow: 'inset -1px 0px 4px rgba(27, 43, 65, 0.1)' }} step={1} min={0}>
+											<NumberInputField className='input' style={{ padding: 0, textAlign: "center", height: 50, width: 54, backgroundColor: '#F5F5F5', borderRadius: '10px 0px 0px 10px', borderWidth: 0 }} />
 											<NumberInputStepper style={{ width: 50, backgroundColor: 'transparent', borderRadius: '10px 10px 10px 10px' }}>
 												<NumberIncrementStepper color="#C94B32" />
 												<NumberDecrementStepper color="#C94B32" style={{ borderTopWidth: 0 }} />
