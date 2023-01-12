b0556f409ea5a8ab70b701512b8b39ddf370b159
rish6ix <rish6ix@rish6ixs-MacBook-Pro.local>
2023-01-12 21:32:08 +0530

    fix

--- src/hooks/useEncryptDecrypt.tsx
+++ src/hooks/useEncryptDecrypt.tsx
@@ -1,7 +1,7 @@
 import { useWeb3React } from "@web3-react/core";
-import { encrypt } from '@metamask/eth-sig-util';
+//import { encrypt } from '@metamask/eth-sig-util';
+import axiosHttp from 'api'
 const ascii85 = require('ascii85');
-const ethUtil = require('ethereumjs-util');
 
 export default () => {
 
@@ -23,20 +23,34 @@ export default () => {
     const encryptMessage = async (message: string) => {
         const publicKey = await getPublicKey()
         if(publicKey) {
-          const encryptedMessage = ethUtil.bufferToHex(
-            Buffer.from(
-              JSON.stringify(
-                encrypt({
-                  publicKey: publicKey.toString('base64'),
-                  data: ascii85.encode(message).toString(),
-                  version: 'x25519-xsalsa20-poly1305',
-                })
-              ),
-              'utf8'
-            )
-          );
-          console.log(encryptedMessage)
-          return encryptedMessage
+          // const encryptedMessage = ethUtil.bufferToHex(
+          //   Buffer.from(
+          //     JSON.stringify(
+          //       encrypt({
+          //         publicKey: publicKey.toString('base64'),
+          //         data: ascii85.encode(message).toString(),
+          //         version: 'x25519-xsalsa20-poly1305',
+          //       })
+          //     ),
+          //     'utf8'
+          //   )
+          // );
+          // const encryptedMessage = Buffer.from(
+          //       JSON.stringify(
+          //         encrypt({
+          //           publicKey: publicKey.toString('base64'),
+          //           data: ascii85.encode(message).toString(),
+          //           version: 'x25519-xsalsa20-poly1305',
+          //         })
+          //       ),
+          //       'utf8'
+          // ).toString('hex')
+          // console.log("encryptedMessage", encryptedMessage)
+          // return encryptedMessage
+          const response = await axiosHttp.post(`utility/encrypt`, { publicKey:  publicKey.toString('base64'), data: ascii85.encode(message).toString() })
+          console.log(response.data.message)
+          if(response && response?.data?.message)
+            return response?.data?.message
         }
         return null
     }
