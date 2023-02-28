5f5cf8dbe17f1c829ac07b04579491c97920e35c
surbhi choubey <surbhichoubey@surbhis-MacBook-Air.local>
2023-02-28 20:25:41 +0530

    Added modal

---
+++ src/pages/NewPages/DashBoard/WalkThrough/WalkThrough.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './WalkThrough.css';
+
+export default function WalkThrough() {
+  return (
+    <div className="confirm-walkthrough">
+    Happy
+    </div>
+  );
+}
