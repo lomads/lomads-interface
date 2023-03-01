7aedbdd265827377135f6f4c585517995165f973
surbhi choubey <surbhichoubey@surbhis-MacBook-Air.local>
2023-03-02 03:24:52 +0530

    user onboarding UI changes

--- src/muiComponents/Modal/index.tsx
+++ src/muiComponents/Modal/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 
@@ -12,22 +12,22 @@ const style = {
   width: '55vw',
   height: '55vh'
 };
-  
-export default ({children, isOpen = false} : { children: React.ReactElement, isOpen: boolean }) => {
+
+export default ({ children, isOpen = false }: { children: React.ReactElement, isOpen: boolean }) => {
   const [open, setOpen] = useState(isOpen);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
   return (
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-       <Box sx={style}>
-           {children}
-        </Box>
-      </Modal>
+    <Modal
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="modal-modal-title"
+      aria-describedby="modal-modal-description"
+    >
+      <Box sx={style}>
+        {children}
+      </Box>
+    </Modal>
   );
 }
