105ac15fc64c90b95d0e91c7b69f2c01ec9d60a7
surbhi2007 <surbhichoubey93@gmail.com>
2023-03-20 19:14:32 +0530

    fixed login

--- src/muiViews/LoginPage/index.tsx
+++ src/muiViews/LoginPage/index.tsx
@@ -1,57 +1,76 @@
 /* eslint-disable */
 import React, { useEffect, useState, useCallback } from "react";
+import { KeyboardArrowDown } from '@mui/icons-material';
+import { Grid, Typography, Box, MenuItem, Menu, Button } from "@mui/material"
 import { get as _get, find as _find, throttle as _throttle, debounce as _debounce } from 'lodash';
 import { useLocation, useNavigate } from "react-router-dom";
-import lomadsfulllogo from "../assets/svg/lomadsfulllogo.svg";
-import humangroup from "../assets/svg/humangroup.svg";
-import metamask2 from "../assets/svg/metamask2.svg";
-import walletconnect from "../assets/svg/walletconnect.svg";
+import LOMADS_LOGO from "../../assets/svg/lomadsfulllogo.svg";
+import METAMASK from "../../assets/svg/metamask2.svg";
 import { useWeb3React } from "@web3-react/core";
-import "../styles/pages/LoginPage.css";
 import { Connector } from "@web3-react/types";
 import { updateSelectedWallet } from "state/user/reducer";
 import { useAppDispatch, useAppSelector } from "state/hooks";
 import { injectedConnection, walletConnectConnection } from "connection";
 import { getConnection } from "connection/utils";
 import { SupportedChainId, SUPPORTED_CHAIN_IDS, CHAIN_IDS_TO_NAMES } from 'constants/chains'
+import { CHAIN_INFO } from 'constants/chainInfo';
 import { updateConnectionError } from "state/connection/reducer";
 import { isChainAllowed, switchChain } from "utils/switchChain";
 import { ethers } from "ethers";
 import { setUser } from "state/dashboard/reducer";
 import Web3Token from 'web3-token';
-import { LeapFrog } from "@uiball/loaders";
 import axiosHttp from '../../api';
 import { getSigner } from 'utils'
-import styled from "styled-components/macro";
-import axios from "axios";
+import CHEERS from 'assets/svg/cheers.svg'
+import { makeStyles } from '@mui/styles';
 
-const HeaderControls = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-self: flex-end;
-`;
+const useStyles = makeStyles((theme: any) => ({
+    root: {
+         height: "100vh",
+         maxHeight: 'fit-content',
+         display: 'flex',
+         flexDirection: 'column',
+         alignItems: 'center',
+         justifyContent: 'center',
+         overflow: 'hidden !important'
+    },
+    logo: {
+        width: 138,
+        height: 81
+    },
+    cheers: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyItems: 'center'
+    },
+    metamaskButton: {
+        height: '111px !important',
+        cursor: 'pointer',
+        alignContent: "inherit",
+        background: "#fff",
+        borderColor: "#c94b32",
+        borderRadius: '10px !important',
+        borderWidth: 0,
+        filter: "drop-shadow(3px 5px 4px rgba(27,43,65,.05)) drop-shadow(-3px -3px 8px rgba(201,75,50,.1)) !important",
+        margin: "10px",
+        padding: 40
+    },
+    select: {
+        background: '#FFF',
+        borderRadius: '10px !important',
+        boxShadow: 'none !important',
+        fontSize: '16px !important',
+        minWidth: 'inherit !importnt',
+        padding: '0px !important'
+    }
+  }));
 
-const HeaderElement = styled.div`
-  display: flex;
-  align-items: center;
 
-  &:not(:first-child) {
-    margin-left: 0.5em;
-  }
-
-  /* addresses safaris lack of support for "gap" */
-  & > *:not(:first-child) {
-    margin-left: 8px;
-  }
-
-  ${({ theme }) => theme.mediaWidth.upToMedium`
-    align-items: center;
-  `};
-`;
-
-
-const LoginPage = (props: any) => {
+export default () => {
 	const dispatch = useAppDispatch();
 	const navigate = useNavigate();
 	const location = useLocation()
@@ -60,7 +79,11 @@ const LoginPage = (props: any) => {
 	const [checkLoading, setCheckLoading] = useState<boolean>(false)
 	const { chainId, connector, account, provider } = useWeb3React();
 	const [preferredChain, setPreferredChain] = useState<number>(SupportedChainId?.GOERLI);
-
+    const classes = useStyles()
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event: React.MouseEvent<HTMLElement>) => setAnchorEl(event.currentTarget);
+    const handleClose = () => setAnchorEl(null);
 
 	console.log('chainId', chainId, connector, account)
 
@@ -72,6 +95,7 @@ const LoginPage = (props: any) => {
 			setPreferredChain(chainId ? SUPPORTED_CHAIN_IDS.indexOf(+chainId) > -1 ? +chainId : SupportedChainId?.GOERLI : SupportedChainId?.GOERLI)
 		}
 	}, [chainId])
+
 
 	const navigateTo = async () => {
 		const activeDao = sessionStorage.getItem('__lmds_active_dao')
@@ -155,62 +179,51 @@ const LoginPage = (props: any) => {
 	}, [window.ethereum])
 
 	return (
-		<>
-			{
-				checkLoading ?
-					<div style={{ backgroundColor: '#FFF', height: '100vh', zIndex: 99999, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-						<div className="logo">
-							<img style={{ marginBottom: 32 }} src={lomadsfulllogo} alt="" />
-						</div>
-						<LeapFrog size={50} color="#C94B32" />
-					</div> : null
-			}
-			<div className={"createDaoLogin"}>
-				{!window.ethereum && <div style={{ position: 'absolute', right: 24, top: 16 }}>
-					<div style={{ opacity: 0.6, fontSize: 14 }}>Metamask not installed</div>
-				</div>}
-				<div>
-					<div className="logo">
-						<img src={lomadsfulllogo} alt="" />
-					</div>
-					<div className="welcomeText1">Hello there!</div>
-					<div className="welcomeText2">Connect Your Wallet</div>
-				</div>
-				{/* <div>
-          <div className="inputFieldTitle">Preferred chain</div>
-        </div> */}
-				<select name="chain" id="chain" value={preferredChain} onChange={e => setPreferredChain(+e.target.value)} className="drop" style={{ width: 250, marginBottom: 16 }}>
-					{
-						SUPPORTED_CHAIN_IDS.map(chain => <option value={+chain}>{CHAIN_IDS_TO_NAMES[chain]}</option>)
-					}
-				</select>
-				<div style={{ zIndex: 1 }} className={"modalbuttons"}>
-					<button
-						key="metamask"
-						className="modalLoginButton"
-						onClick={() => nextLogin(injectedConnection.connector)}
-					>
-						<img src={metamask2} style={{ padding: 40 }} alt="MetaMask" />
-					</button>
-					<button
-						key="walletconnect"
-						className="modalLoginButton"
-						onClick={() => nextLogin(walletConnectConnection.connector)}
-					>
-						<img
-							src={walletconnect}
-							style={{ padding: 40 }}
-							alt="WalletConnect"
-						/>
-					</button>
-					{/* <Aikon /> */}
-				</div>
-				<div className="humangroup">
-					<img src={humangroup} alt="human group" />
-				</div>
-			</div>
-		</>
+        <>
+        <Grid container className={classes.root}>
+            <Grid xs={12} item display="flex" flexDirection="column" alignItems="center">
+                <Box zIndex={0} position="absolute" bottom={0}>
+                    <img src={CHEERS} style={{ marginBottom: '-5px' }} />
+                </Box>
+                <Box mb={12} mt={3}>
+                    <img src={LOMADS_LOGO} />
+                </Box>
+                <Typography my={1} variant="subtitle1">Hello there !</Typography>
+                <Typography mt={2} mb={4} color="primary" variant="h2">Connect Your Wallet</Typography>
+                <Button onClick={() => nextLogin(injectedConnection.connector)} className={classes.metamaskButton} variant='contained' color='secondary'>
+                    <img src={METAMASK} />
+                </Button>
+                <Box mt={4} display="flex" flexDirection="row" alignItems="center">
+                    <Typography variant='body1' fontWeight="bold" mr={2}>Select Blockchain:</Typography>
+                    <Button onClick={() => nextLogin(walletConnectConnection.connector)} aria-controls={open ? 'fade-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} className={classes.select} variant="contained" color="secondary" disableElevation startIcon={<img style={{ width: 18, height: 18 }} src={CHAIN_INFO[preferredChain].logoUrl}/>} endIcon={<KeyboardArrowDown />}>
+                        { CHAIN_INFO[preferredChain].label }
+                    </Button>
+                    <Menu
+                        MenuListProps={{
+                        'aria-labelledby': 'fade-button',
+                        }}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                    >
+                        {
+                            SUPPORTED_CHAIN_IDS.map((sc: any) => 
+                                <MenuItem style={{ textTransform: 'uppercase' }} onClick={() => { handleClose() }}>
+                                    <img style={{ marginRight: '8px', width: 18, height: 18 }} src={CHAIN_INFO[sc].logoUrl} />{ CHAIN_INFO[sc].label }</MenuItem>)
+                        }
+                    </Menu>
+                </Box>
+                <Box height={200}></Box>
+            </Grid>
+        </Grid>
+    </>
 	);
 };
-
-export default LoginPage;
