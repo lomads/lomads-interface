869e6cbfb7520a0b1fadb6eeda9b12215e451652
rish6ix <rish6ix@rish6ix-macBook-pro.local>
2023-04-15 11:16:20 +0530

    fix

--- src/pages/NewPages/DashBoard/Task/CreateTask.jsx
+++ src/pages/NewPages/DashBoard/Task/CreateTask.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useMemo, useRef } from 'react';
-import { find as _find, get as _get, debounce as _debounce } from 'lodash';
+import { find as _find, get as _get, debounce as _debounce, orderBy as _orderBy } from 'lodash';
 import './CreateTask.css';
 import { CgClose } from 'react-icons/cg'
 import createProjectSvg from '../../../../assets/svg/createProject.svg';
@@ -479,9 +479,9 @@ const CreateTask = ({ toggleShowCreateTask, selectedProject }) => {
                                             >
                                                 <option value={null}>Select member</option>
                                                 {
-                                                    eligibleReviewers.map((item, index) => {
+                                                    _orderBy(eligibleReviewers, er => er.member.name).map((item, index) => {
                                                         return (
-                                                            <option value={`${item.member._id}`}>{item.member.name}</option>
+                                                            <option value={`${item.member._id}`}>{item.member.name} ({ beautifyHexToken(item?.member?.wallet) })</option>
                                                         )
                                                     })
                                                 }
